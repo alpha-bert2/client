e19e897264ba5bf3931532512c999dff27e4c5ac
Herbert <herbertofori29@gmail.com>
2026-05-24 09:21:29 +1200

    oranised my navBar

---
+++ js/yohane.js
@@ -0,0 +1,8 @@
+const openMenu = () =>{
+    const dropdown = document.querySelector('.dropdown');
+    dropdown.style.display = 'block';
+}
+const closeMenu = () =>{
+    const dropdown = document.querySelector('.dropdown');
+    dropdown.style.display = 'none';
+}
